520aa6b829d9ccb081dd7898defe6a7e7c5ebaee
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-18 14:33:01 +0200

    Snack 3

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filter_by_initial(listStrings, char) {
+    let res = []
+    for (i = 0; i <= listStrings.length - 1; i++) {
+        if (listStrings[i][0] === char) {
+            res.push(listStrings[i])
+        }
+    }
+    return res
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filter_by_initial(names,'A'))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
