77c06d4edc4f435681d866ddbdf7a5643943fa72
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-18 14:24:20 +0200

    Snack 2

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const initials = (listNames) => {
+    let res = []
+    for (let i = 0; i <= listNames.length - 1; i++) {
+        res.push(listNames[i][0])
+    }
+    return res
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(initials(names))
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
