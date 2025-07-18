63be5be9130285e7d1a3e4721138bb1180be26f0
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-18 14:41:29 +0200

    Snack 4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+const vowels = ['a','e','i','o','u']
+const count_vowels = (parola) => {
+    let count = 0
+    const lowercased = parola.toLowerCase()
+    for (i = 0; i <= lowercased.length -1; i++) {
+        for (j = 0; j <= vowels.length -1; j++) {
+            if (lowercased[i] === vowels[j]) {
+                count += 1
+            }
+        }
+    }
+    return count
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(count_vowels(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
